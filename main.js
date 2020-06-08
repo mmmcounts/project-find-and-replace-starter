7208ae751b51bb2ac691ed596ab6bf14401c2858
Mike <mmmcounts@gmail.com>
2020-06-08 16:39:23 -0500

    it is functional

--- main.js
+++ main.js
@@ -10,18 +10,40 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // this array.
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+function getCellElements(currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
 
 // YOUR CODE GOES HERE
+replaceAllButton.addEventListener('click', function () {
+    let theTypo = findInput.value
+    let theEdit = replaceInput.value
+    for (let itemIndex = 0; itemIndex < rowElements.length; itemIndex += 1) {
+        let actualRowElement = rowElements[itemIndex];
+        //console.log(actualRowElement)
+        let cellElements = getCellElements(actualRowElement)
+        //console.log(cellContent) this is like an array inside the other array
+        for (let cellIndex = 0; cellIndex < cellElements.length; cellIndex += 1) {
+            //console.log(cellContent)
+            let actualCellElement = cellElements[cellIndex]
+            //console.log(actualCellElement)
+            let innerHTML = actualCellElement.innerHTML
+            //console.log(innerHTML)
 
+            //regex is a little too advanced for use right now
+            while (innerHTML.includes(theTypo)) {
+                innerHTML = innerHTML.replace(theTypo, theEdit)
+            }
+            actualCellElement.innerHTML = innerHTML
+        }
+    }
+})
 
 // One last thing: dedicate very careful attention to using variables and
 // naming them accurately.
